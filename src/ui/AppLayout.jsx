16065d0400979b658839f331c68b40a5d106b89f
kiethohoai <kietho.060193@gmail.com>
2024-07-14 21:34:26 +0700

    008 Displaying a Loading Indicator

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,10 +1,17 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import CartOverview from "../features/cart/CartOverview";
 import Header from "./Header";
+import Loader from "./Loader";
 
 function AppLayout() {
+  const navigate = useNavigation();
+  console.log("🚀CHECK  navigate =", navigate);
+  const isLoading = navigate.state === "loading";
+
   return (
-    <div>
+    <div className="layout">
+      {isLoading && <Loader />}
+
       <Header />
 
       <main>
