62e364742f238ac83970d491aeaafe6011e75bd0
kiethohoai <kietho.060193@gmail.com>
2024-07-15 17:11:48 +0700

    008 Using Flexbox

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="hidden text-sm font-semibold">David</div>;
+  return <div className="hidden text-sm font-semibold md:block">David</div>;
 }
 
 export default Username;
