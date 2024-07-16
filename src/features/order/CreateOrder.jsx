16b5d6589b8a12c4c0f65f26f5bb97254a1c41b3
kiethohoai <kietho.060193@gmail.com>
2024-07-16 10:35:06 +0700

    013 Reusing Styles With React Components

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,6 +1,7 @@
 // import { useState } from "react";
 import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
+import Button from "../../ui/Button";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -86,12 +87,13 @@ export default function CreateOrder() {
 
         <div>
           <input hidden name="cart" defaultValue={JSON.stringify(cart)} />
-          <button
+          {/* <button
             disabled={isSubmitting}
             className="inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed"
           >
             {isSubmitting ? "Placing order..." : "Order now"}
-          </button>
+          </button> */}
+          <Button>ORDER NOW</Button>
         </div>
       </Form>
     </div>
